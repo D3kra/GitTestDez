44a0f4261d9924e0a532dc88dc4daf49aee0fde3
lboeke <leonard.boeke@fh-bielefeld.de>
2021-12-16 14:45:44 +0100

    init

---
+++ src/firebase-config.js
@@ -0,0 +1,20 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore} from "firebase/firestore"
+import { getAuth } from "firebase/auth";
+
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCfsrWxrVv-j4Vc_LfcBPyVVwmMzopLdx0",
+  authDomain: "daytopat-add83.firebaseapp.com",
+  projectId: "daytopat-add83",
+  storageBucket: "daytopat-add83.appspot.com",
+  messagingSenderId: "446229225547",
+  appId: "1:446229225547:web:137f869dfbe5de13302dce"
+};
+
+const app = initializeApp(firebaseConfig);
+
+const db = getFirestore();
+
+export const auth = getAuth(app);
+export { db }
